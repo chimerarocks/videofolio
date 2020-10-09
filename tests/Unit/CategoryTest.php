7a6a64cf7ee33f3b8f26eee662d9fba1bce9a1c7
chimerarocks <joaopedrodslv@gmail.com>
2020-10-09 11:00:16 -0300

    create category tests

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    public function testFillable()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+        $this->assertEquals($fillable, $category->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+    public function testKeyTypeAttribute()
+    {
+        $category = new Category();
+        $this->assertEquals('string', $category->getKeyType());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $category      = new Category();
+        $dates         = ['deleted_at', 'created_at', 'updated_at'];
+        $categoryDates = $category->getDates();
+        sort($dates);
+        sort($categoryDates);
+        $this->assertEquals($dates, $categoryDates);
+    }
+
+}
