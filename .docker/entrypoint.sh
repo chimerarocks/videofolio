16b898fbfd98ec443e139a1294027c69ff9ee315
chimerarocks <joaopedrodslv@gmail.com>
2020-10-15 10:49:45 -0300

    add ci cd configuration

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+if [ ! -e .env ]
+ then cp .env.example .env
+fi
+
+if [ ! -e .env.testing ]
+ then cp .env.testing.example .env.testing
+fi
+
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
